f414a002fbac5284881b577ec67fb21c23e217f4
Devvart1 <devvartkumarkumar@gmail.com>
2024-08-25 13:31:09 +0530

    feat: Add comment and like models

---
+++ src/models/like.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+    likedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+export const Like = mongoose.model("Like", likeSchema);
