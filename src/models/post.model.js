f437571daa422901b40c9a37d93b97324648db5b
Devvart1 <devvartkumarkumar@gmail.com>
2024-08-25 13:44:28 +0530

    feat: Add playlist and post models

---
+++ src/models/post.model.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+const postSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+export const Post = mongoose.model("Post", postSchema);
