b5fcc28fe64f207f5d22a149bee65ac57e3ac788
Devvart1 <devvartkumarkumar@gmail.com>
2024-09-11 17:30:54 +0530

    Refactor subscription controller and fix typo in toggleSubscription function

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,61 +2,38 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { Subscription } from "../models/subscription.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import User from "../models/user.model.js";
-import mongoose from "mongoose";
+
 const toggleSubscription = asyncHandler(async (req, res) => {
-  const { channelId } = req.params;
+  const { chennelId } = req.params;
   const userId = req.user._id;
-  console.log(channelId, userId);
-  if (!channelId || !userId) {
+
+  if (!chennelId || !userId) {
     return res.status(400).json({ message: "Invalid Request" });
   }
-  const channel = await User.findById(channelId);
+  const chennel = await User.findById(chennelId);
 
-  if (!channel) {
+  if (!chennel) {
     return res.status(404).json({ message: "Invalid Channel" });
   }
-  const findAllSubscriptions = await Subscription.find();
-  console.log(findAllSubscriptions);
-  const subscriptions = await User.aggregate([
-    {
-      $match: {
-        _id: new mongoose.Types.ObjectId(req.user._id),
-      },
-    },
-    {
-      $lookup: {
-        from: "subscriptions",
-        localField: "_id",
-        foreignField: "subscriber",
-        as: "subscribedTo",
-      },
-    },
-    {
-      $addFields: {
-        isSubscribed: {
-          $in: [channelId, "$subscribedTo"],
-        },
-      },
-    },
-    {
-      $project: {
-        isSubscribed: 1,
-      },
-    },
-  ]);
-  // console.log(subscriptions);
 
-  if (subscriptions[0]?.isSubscribed) {
-    await Subscription.findByIdAndDelete(isSubscribed._id);
+  const subscriptions = await Subscription.find({
+    subscriber: userId,
+    chennel: chennelId,
+  });
+
+  if (subscriptions.length > 0) {
+    await Subscription.findByIdAndDelete(subscriptions[0]?._id);
     console.log("Unsubscribed");
     return res
       .status(200)
       .json(new ApiResponse(200, "Unsubscribed Successfully"));
   }
+
   const newSubscription = await Subscription.create({
     subscriber: userId,
-    channel: channelId,
+    chennel: chennelId,
   });
+
   console.log("Subscribed");
   res.status(200).json(new ApiResponse(200, "Subscribed Successfully"));
 });
