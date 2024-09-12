caff4759c47febbcd7ca046e6a08ca578114eb17
Devvart1 <devvartkumarkumar@gmail.com>
2024-09-12 14:03:42 +0530

    Refactor subscription controller, fix typo, and add new functionality

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -13,7 +13,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   const chennel = await User.findById(chennelId);
 
   if (!chennel) {
-    return res.status(404).json({ message: "Invalid Channel" });
+    return res.status(404).json({ message: "Invalid chennel" });
   }
 
   const subscriptions = await Subscription.find({
@@ -38,4 +38,52 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   res.status(200).json(new ApiResponse(200, "Subscribed Successfully"));
 });
 
-export { toggleSubscription };
+// controller to return subscriber list of a chennel
+const getUserchennelSubscribers = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    return res.status(404).json({ message: "Invalid chennel" });
+  }
+
+  // const subscribers = await Subscription.find({ chennel: req.user._id });
+  // const subscriberIds = subscribers.map((sub) => sub.subscriber);
+  const Subscribers = await User.aggregate([
+    {
+      $match: {
+        _id: user._id,
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "chennel",
+        as: "subscribers",
+      },
+    },
+    {
+      $project: {
+        subscribers: 1,
+      },
+    },
+  ]);
+  const subscriberIds = Subscribers[0]?.subscribers.map(
+    (sub) => sub.subscriber
+  );
+  console.log(Subscribers.subscriberIds);
+  res.status(200).json(new ApiResponse(200, "Subscribers", subscriberIds));
+});
+
+// controller to return chennel list to which user has subscribed
+const getSubscribedchennels = asyncHandler(async (req, res) => {
+  const user = await User.find(req.user._id);
+  if (!user) {
+    return res.status(404).json({ message: "Invalid chennel" });
+  }
+
+  const channels = await Subscription.find({ subscriber: req.user._id });
+  const chennelIds = channels.map((sub) => sub.chennel);
+  // console.log(chennelIds);
+  res.status(200).json(new ApiResponse(200, "Chennels", chennelIds));
+});
+export { toggleSubscription, getUserchennelSubscribers, getSubscribedchennels };
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import {
+  toggleSubscription,
+  getSubscribedchennels,
+  getUserchennelSubscribers,
+} from "../controllers/subscription.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/subscribe/:chennelId").post(toggleSubscription);
+router.route("/c/:chennelId").get(toggleSubscription);
+router.route("/getSubscribedchennels").get(getSubscribedchennels);
+router.route("/getUserchennelSubscribers").get(getUserchennelSubscribers);
 
 export default router;
